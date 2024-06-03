38749507b5960e82375dc96d0d1458a2d8157ca0
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-03 16:50:27 +0300

    fix: typo

--- ecommerce/lib/features/product/data/data_sources/local/local_data_source.dart
+++ ecommerce/lib/features/product/data/data_sources/local/local_data_source.dart
@@ -4,6 +4,6 @@ abstract class ProductLocalDataSource {
   Future<List<ProductModel>> getProducts();
   Future<ProductModel> getProduct(String id);
   Future<void> deleteProduct(String id);
-  Future<void> cacheProduct(List<ProductModel> product);
-  Future<void> cacheProducts(List<ProductModel> product);
+  Future<void> cacheProduct(ProductModel product);
+  Future<void> cacheProducts(List<ProductModel> products);
 }
