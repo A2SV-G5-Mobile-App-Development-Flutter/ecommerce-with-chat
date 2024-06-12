15904a213dd0f74c32e77262af25693395bb262d
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-12 03:20:15 +0300

    feat: add bloc consumption

--- ecommerce/lib/features/product/presentation/pages/home.dart
+++ ecommerce/lib/features/product/presentation/pages/home.dart
@@ -1,98 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/presentation/routes/routes.dart';
-import '../../domain/entities/product.dart';
+
+import '../../../../core/presentation/widgets/snackbar.dart';
+import '../bloc/product/product_bloc.dart';
 import '../widgets/widgets.dart';
 
 class HomePage extends StatelessWidget {
-  final products = const [
-    Product(
-        id: 'id',
-        name: 'HP Victus',
-        description: 'Personal Computer',
-        price: 123.45,
-        imageUrl:
-            'https://www.omen.com/content/dam/sites/omen/worldwide/laptops/2022-victus-15-intel/Hero%20Image%203.png'),
-    Product(
-        id: 'id',
-        name: 'HP Victus 16',
-        description: 'Gaming Laptop',
-        price: 123.45,
-        imageUrl:
-            'https://www.omen.com/content/dam/sites/omen/worldwide/laptops/2022-victus-15-intel/Hero%20Image%203.png'),
-    Product(
-        id: 'id',
-        name: 'HP Victus 16',
-        description: 'Gaming Laptop',
-        price: 123.45,
-        imageUrl:
-            'https://www.omen.com/content/dam/sites/omen/worldwide/laptops/2022-victus-15-intel/Hero%20Image%203.png'),
-    Product(
-        id: 'id',
-        name: 'HP Victus 16',
-        description: 'Gaming Laptop',
-        price: 123.45,
-        imageUrl:
-            'https://www.omen.com/content/dam/sites/omen/worldwide/laptops/2022-victus-15-intel/Hero%20Image%203.png'),
-  ];
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
-          child: Column(
-            children: [
-              // Header
-              const UserHeader(userName: 'John'),
-              const SizedBox(height: 20),
+    return BlocListener<ProductsBloc, ProductsState>(
+      listener: (context, state) {
+        if (state is ProductsFailure) {
+          showError(context, state.message);
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
+            child: Column(
+              children: [
+                // Header
+                const UserHeader(userName: 'John'),
+                const SizedBox(height: 20),
 
-              // Title
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Available Products',
-                      style: Theme.of(context).textTheme.titleLarge),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.search,
-                      color: Theme.of(context).disabledColor,
+                // Title
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Available Products',
+                        style: Theme.of(context).textTheme.titleLarge),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.search,
+                        color: Theme.of(context).disabledColor,
+                      ),
+                      iconSize: 20,
                     ),
-                    iconSize: 20,
-                  ),
-                ],
-              ),
-
-              // Product List
-              Expanded(
-                child: ListView.builder(
-                  itemCount: products.length,
-                  itemBuilder: (context, index) {
-                    final product = products[index];
-
-                    return ProductCard(
-                        product: product,
-                        onProductSelected: (product) {
-                          context.push(Routes.productDetail, extra: product);
-                        });
-                  },
+                  ],
                 ),
-              ),
-            ],
+
+                // Product List
+                Expanded(
+                  child: BlocBuilder<ProductsBloc, ProductsState>(
+                    builder: (context, state) {
+                      return RefreshIndicator(
+                        onRefresh: () async {
+                          context
+                              .read<ProductsBloc>()
+                              .add(ProductsLoadRequested());
+                        },
+                        child: ListView.builder(
+                          itemCount: state.products.length,
+                          itemBuilder: (context, index) {
+                            final product = state.products[index];
+
+                            return ProductCard(
+                                product: product,
+                                onProductSelected: (product) {
+                                  context.push(Routes.productDetail,
+                                      extra: product);
+                                });
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
 
-      // Floating Action Button
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.push(Routes.addProduct);
-        },
-        child: const Icon(Icons.add),
+        // Floating Action Button
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.push(Routes.addProduct);
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
--- ecommerce/lib/main.dart
+++ ecommerce/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'bloc_observer.dart';
 import 'core/presentation/routes/router.dart';
+import 'features/product/presentation/bloc/product/product_bloc.dart';
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
@@ -10,6 +12,7 @@ Future<void> main() async {
   Bloc.observer = SimpleBlocObserver();
 
   await di.init();
+
   runApp(const App());
 }
 
@@ -18,58 +21,62 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Products',
-      theme: ThemeData(
-        colorScheme: const ColorScheme(
-          brightness: Brightness.light,
-          primary: Color(0xFF3E50F3),
-          secondary: Color(0xFF0C8CE9),
-          surface: Color(0xFFF1EEEE),
-          background: Color(0xFFFFFFFF),
-          error: Color(0xFFB00020),
-          onPrimary: Color(0xFFFFFFFF),
-          onSecondary: Color(0xFFFFFFFF),
-          onSurface: Color(0xFF000000),
-          onBackground: Color(0xFF000000),
-          onError: Color(0xFFFFFFFF),
+    return BlocProvider<ProductsBloc>(
+      create: (context) =>
+          di.serviceLocator<ProductsBloc>()..add(ProductsLoadRequested()),
+      child: MaterialApp.router(
+        title: 'Products',
+        theme: ThemeData(
+          colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            primary: Color(0xFF3E50F3),
+            secondary: Color(0xFF0C8CE9),
+            surface: Color(0xFFF1EEEE),
+            background: Color(0xFFFFFFFF),
+            error: Color(0xFFB00020),
+            onPrimary: Color(0xFFFFFFFF),
+            onSecondary: Color(0xFFFFFFFF),
+            onSurface: Color(0xFF000000),
+            onBackground: Color(0xFF000000),
+            onError: Color(0xFFFFFFFF),
+          ),
+
+          //
+          //! Font
+          fontFamily: 'Poppins',
+          textTheme: const TextTheme(
+            titleSmall: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+            titleMedium: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+            titleLarge: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+            bodySmall: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+            bodyMedium: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+            bodyLarge: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
         ),
 
         //
-        //! Font
-        fontFamily: 'Poppins',
-        textTheme: const TextTheme(
-          titleSmall: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-          ),
-          titleMedium: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
-          ),
-          titleLarge: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-          ),
-          bodySmall: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-          ),
-          bodyLarge: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w400,
-          ),
-        ),
+        debugShowCheckedModeBanner: false,
+
+        routerConfig: router,
       ),
-
-      //
-      debugShowCheckedModeBanner: false,
-
-      routerConfig: router,
     );
   }
 }
