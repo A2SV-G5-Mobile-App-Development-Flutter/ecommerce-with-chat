95f657a711fe1192fc03449da70bb88f78bfd2e2
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-08-15 15:29:27 +0300

    feat: update api url

--- ecommerce/lib/core/constants/constants.dart
+++ ecommerce/lib/core/constants/constants.dart
@@ -1,5 +1,6 @@
 // APIs
-const String baseUrl = 'http://192.168.188.101:3000/api/v1';
+const String baseUrl =
+    'https://g5-flutter-learning-path-be.onrender.com/api/v1';
 const Map<String, String> defaultHeaders = {
   'Content-Type': 'application/json; charset=UTF-8'
 };
