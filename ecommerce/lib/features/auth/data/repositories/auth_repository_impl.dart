6b96667c724d82aba1e8062487a35922cc223263
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-08-16 02:21:51 +0300

    feat: update access token on login and on user load

--- ecommerce/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ ecommerce/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/exception.dart';
 import '../../../../core/error/failure.dart';
+import '../../../../core/network/http.dart';
 import '../../../../core/network/network_info.dart';
 import '../../domain/entities/authenticated_user.dart';
 import '../../domain/repositories/auth_repository.dart';
@@ -15,8 +16,10 @@ class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
   final AuthLocalDataSource localDataSource;
   final NetworkInfo networkInfo;
+  final HttpClient client;
 
   AuthRepositoryImpl({
+    required this.client,
     required this.remoteDataSource,
     required this.localDataSource,
     required this.networkInfo,
@@ -31,6 +34,8 @@ class AuthRepositoryImpl implements AuthRepository {
       try {
         final accessToken = await remoteDataSource
             .login(LoginModel(email: email, password: password));
+
+        client.authToken = accessToken.token;
 
         final user = await remoteDataSource.getCurrentUser();
 
@@ -55,6 +60,7 @@ class AuthRepositoryImpl implements AuthRepository {
   Future<Either<Failure, Unit>> logout() async {
     try {
       await localDataSource.clear();
+      client.authToken = '';
       return const Right(unit);
     } on CacheException {
       return const Left(CacheFailure('Unable to logout'));
@@ -65,6 +71,7 @@ class AuthRepositoryImpl implements AuthRepository {
   Future<Either<Failure, AuthenticatedUser>> getCurrentUser() async {
     try {
       final user = await localDataSource.getUser();
+      client.authToken = user.accessToken;
       return Right(user);
     } on CacheException {
       return Left(AuthFailure.tokenExpired());
@@ -82,7 +89,9 @@ class AuthRepositoryImpl implements AuthRepository {
         await remoteDataSource.register(
           RegisterModel(name: name, email: email, password: password),
         );
-        return await login(email: email, password: password);
+        final user = await login(email: email, password: password);
+        client.authToken = user.fold((l) => '', (r) => r.accessToken);
+        return user;
       } on ServerException {
         return const Left(ServerFailure('Unable to register'));
       }
